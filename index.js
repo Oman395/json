80d103d31c901fd2d26952603b013197b80b673c
Oran Hartley <oranroha@gmail.com>
2021-09-27 09:39:38 -0400

    TODO: fix error messages, add errs for user error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (!fs.existsSync("./data.json")) {
 }
 var usrData = JSON.parse(fs.readFileSync("./data.json"));
 if (!fs.existsSync("./files")) {
-    fs.mkdir("./files", () => {});
+    fs.mkdir("./files", () => { });
 }
 if (!usrData.actPath) {
     usrData.actPath = "";
@@ -61,204 +61,206 @@ function loop() {
                     loop();
             }
         });
-    }
-    var q = "What would you like to do? If you don't know what to do, type 'H'!";
-    if (usrData.actPath.length > 0) {
-        q += " "
-    }
-    usrData.actPath.forEach((item) => {
-        if (usrData.actPath.length - 1 > usrData.actPath.indexOf(item)) {
-            q += `${item}.`;
-        } else {
-            q += `${item}`;
+    } else {
+        var q = "What would you like to do? If you don't know what to do, type 'H'!";
+        if (usrData.actPath.length > 0) {
+            q += " "
         }
-    });
-    q += " > ";
-    rl.question(q, (answer) => {
-        var args = answer.split(" ");
-        args[0] = args[0].toUpperCase();
-        args[0] = args[0][0];
-        switch (args[0]) {
-            case "H":
-                if (!args[1]) {
-                    help(1);
-                    rl.close();
-                    loop();
-                } else {
-                    help(2, args[1]);
-                    rl.close();
-                    if (args[1] != "E") {
+        usrData.actPath.forEach((item) => {
+            if (usrData.actPath.length - 1 > usrData.actPath.indexOf(item)) {
+                q += `${item}.`;
+            } else {
+                q += `${item}`;
+            }
+        });
+        q += " > ";
+        rl.question(q, (answer) => {
+            var args = answer.split(" ");
+            args[0] = args[0].toUpperCase();
+            args[0] = args[0][0];
+            switch (args[0]) {
+                case "H":
+                    if (!args[1]) {
+                        help(1);
+                        rl.close();
                         loop();
                     } else {
-                        fs.writeFileSync("./data.json", JSON.stringify(usrData));
-                    }
-                }
-                break;
-            case "E":
-                console.log("Goodbye!");
-                fs.writeFileSync("./data.json", JSON.stringify(usrData));
-                fs.writeFileSync(usrData.resumePath, JSON.stringify(data));
-                rl.close();
-                break;
-            default:
-                console.log(`Sorry, I didn't understand ${args[0]}. Press h if you don't know what to do.`);
-                rl.close();
-                loop();
-                break;
-            case "N":
-                if (args[1]) {
-                    if (fs.existsSync(`./files/${args[1]}.json`)) {
-                        data = JSON.parse(fs.readFileSync(`./files/${args[1]}.json`));
-                    } else {
-                        data = {};
-                        fs.writeFileSync(`./files/${args[1]}.json`, JSON.stringify(data));
-                    }
-                    usrData.resumePath = `./files/${args[1]}.json`;
-                    usrData.name = args[1];
-                    fs.writeFileSync("./data.json", JSON.stringify(usrData));
-                    rl.close();
-                    loop();
-                } else {
-                    console.log("ERR: Please specify a file");
-                    rl.close();
-                    loop();
-                }
-                break;
-            case "M":
-                var string = JSON.stringify(data);
-                var y = 0;
-                var map = `${usrData.name}`;
-                var pushAmnt = 0;
-                for (let i = 0; i < string.length; i++) {
-                    switch (string[i]) {
-                        case "{":
-                            map += "\n";
-                            pushAmnt++;
-                            for (let i = 0; i < pushAmnt; i++) {
-                                map += " ";
-                            }
-                            break;
-                        case "}":
-                            pushAmnt--;
-                            break;
-                        case ",":
-                            map += "\n";
-                            for (let i = 0; i < pushAmnt; i++) {
-                                map += " ";
-                            }
-                            break;
-                        case '"':
-                            break;
-                        default:
-                            map += string[i];
-                    }
-                }
-                var q = "";
-                usrData.actPath.forEach((item) => {
-                    q += `.${item}`;
-                });
-                console.log(map);
-                console.log(`Current path is ${usrData.name}${q}`)
-                rl.close();
-                loop();
-                break;
-            case "G":
-                if (args[1]) {
-                    try {
-                        usrData.actPath.push(args[1]);
-                    } catch {
-                        console.log("Please specify a valid path.");
-                    }
-                } else {
-                    console.log("Please specify a path.");
-                }
-                rl.close();
-                loop();
-                break;
-            case "B":
-                usrData.actPath.pop();
-                rl.close();
-                loop();
-                break;
-            case "A":
-                var currentData = data;
-                try {
-                    usrData.actPath.forEach((pathItem) => {
-                        currentData = currentData[pathItem];
-                    });
-                } catch {
-                    usrData.actPath = [];
-                    currentData = data;
-                }
-                if (args[1]) {
-                    currentData[args[1]] = {};
-                    fs.writeFileSync(usrData.resumePath, JSON.stringify(data));
-                } else {
-                    console.log("Please specify a name.");
-                }
-                rl.close();
-                loop();
-                break;
-            case "S":
-                if (args[1] && args[2]) {
-                    var currentData = data;
-                    try {
-                        usrData.actPath.forEach((pathItem) => {
-                            currentData = currentData[pathItem];
-                        });
-                    } catch {
-                        usrData.actPath = [];
-                        currentData = data;
-                    }
-                    if (!currentData[args[1]]) {
-                        currentData[args[1]] = "";
-                    }
-                    if (currentData[args[1]].length || Object.keys(currentData[args[1]]).length == 0) {
-                        currentData[args[1]] = args[2];
-                        for (let i = 3; i < args.length; i++) {
-                            currentData[args[1]] += ` ${args[i]}`;
-                        }
-                    } else {
-                        console.log("Please only write to an index without sub indexes.");
-                    }
-                } else if (args[1]) {
-                    console.log("Please add a name.");
-                } else if (args[2]) {
-                    console.log("Please add data.");
-                } else {
-                    console.log("Please add the index, and data.");
-                }
-                rl.close();
-                loop();
-                break;
-            case "D":
-                if (args[1]) {
-                    var currentData = data;
-                    try {
-                        usrData.actPath.forEach((pathItem) => {
-                            currentData = currentData[pathItem];
-                        });
-                    } catch {
-                        usrData.actPath = [];
-                        currentData = data;
-                    }
-                    if (currentData && currentData[args[1]]) {
-                        if (currentData[args[1]].length || Object.keys(currentData[args[1]]).length == 0) {
-                            delete currentData[args[1]];
+                        help(2, args[1]);
+                        rl.close();
+                        if (args[1] != "E") {
+                            loop();
                         } else {
-                            console.log("Sorry, please don't delete items with sub items.");
+                            fs.writeFileSync("./data.json", JSON.stringify(usrData));
+                        }
+                    }
+                    break;
+                case "E":
+                    console.log("Goodbye!");
+                    fs.writeFileSync("./data.json", JSON.stringify(usrData));
+                    fs.writeFileSync(usrData.resumePath, JSON.stringify(data));
+                    rl.close();
+                    break;
+                default:
+                    console.log(`Sorry, I didn't understand ${args[0]}. Press h if you don't know what to do.`);
+                    rl.close();
+                    loop();
+                    break;
+                case "N":
+                    if (args[1]) {
+                        if (fs.existsSync(`./files/${args[1]}.json`)) {
+                            data = JSON.parse(fs.readFileSync(`./files/${args[1]}.json`));
+                        } else {
+                            data = {};
+                            fs.writeFileSync(`./files/${args[1]}.json`, JSON.stringify(data));
+                        }
+                        usrData.resumePath = `./files/${args[1]}.json`;
+                        usrData.name = args[1];
+                        fs.writeFileSync("./data.json", JSON.stringify(usrData));
+                        rl.close();
+                        loop();
+                    } else {
+                        console.log("ERR: Please specify a file");
+                        rl.close();
+                        loop();
+                    }
+                    break;
+                case "M":
+                    var string = JSON.stringify(data);
+                    var y = 0;
+                    var map = `${usrData.name}`;
+                    var pushAmnt = 0;
+                    for (let i = 0; i < string.length; i++) {
+                        switch (string[i]) {
+                            case "{":
+                                map += "\n";
+                                pushAmnt++;
+                                for (let i = 0; i < pushAmnt; i++) {
+                                    map += " ";
+                                }
+                                break;
+                            case "}":
+                                pushAmnt--;
+                                break;
+                            case ",":
+                                map += "\n";
+                                for (let i = 0; i < pushAmnt; i++) {
+                                    map += " ";
+                                }
+                                break;
+                            case '"':
+                                break;
+                            default:
+                                map += string[i];
+                        }
+                    }
+                    var q = "";
+                    usrData.actPath.forEach((item) => {
+                        q += `.${item}`;
+                    });
+                    console.log(map);
+                    console.log(`Current path is ${usrData.name}${q}`)
+                    rl.close();
+                    loop();
+                    break;
+                case "G":
+                    if (args[1]) {
+                        try {
+                            usrData.actPath.push(args[1]);
+                        } catch {
+                            console.log("Please specify a valid path.");
                         }
                     } else {
-                        console.log("Sorry, that index does not exist.");
+                        console.log("Please specify a path.");
                     }
-                } else {
-                    console.log("Please add an index.");
-                }
-                rl.close();
-                loop();
-                break;
-        }
-    });
+                    rl.close();
+                    loop();
+                    break;
+                case "B":
+                    usrData.actPath.pop();
+                    rl.close();
+                    loop();
+                    break;
+                case "A":
+                    var currentData = data;
+                    console.log(usrData.actPath);
+                    usrData.actPath.forEach((pathItem) => {
+                        if (pathItem) {
+                            console.log(pathItem);
+                            console.log(currentData);
+                            currentData = currentData[pathItem];
+                        }
+                    });
+                    if (args[1]) {
+                        currentData[args[1]] = {};
+                        fs.writeFileSync(usrData.resumePath, JSON.stringify(data));
+                    } else {
+                        console.log("Please specify a name.");
+                    }
+                    rl.close();
+                    loop();
+                    break;
+                case "S":
+                    if (args[1] && args[2]) {
+                        var currentData = data;
+                        try {
+                            usrData.actPath.forEach((pathItem) => {
+                                currentData = currentData[pathItem];
+                            });
+                        } catch {
+                            usrData.actPath = [];
+                            currentData = data;
+                        }
+                        if (!currentData[args[1]]) {
+                            currentData[args[1]] = "";
+                        }
+                        if (currentData[args[1]].length || Object.keys(currentData[args[1]]).length == 0) {
+                            currentData[args[1]] = args[2];
+                            for (let i = 3; i < args.length; i++) {
+                                currentData[args[1]] += ` ${args[i]}`;
+                            }
+                        } else {
+                            console.log("Please only write to an index without sub indexes.");
+                        }
+                    } else if (args[1]) {
+                        console.log("Please add a name.");
+                    } else if (args[2]) {
+                        console.log("Please add data.");
+                    } else {
+                        console.log("Please add the index, and data.");
+                    }
+                    rl.close();
+                    loop();
+                    break;
+                case "D":
+                    if (args[1]) {
+                        var currentData = data;
+                        try {
+                            usrData.actPath.forEach((pathItem) => {
+                                currentData = currentData[pathItem];
+                            });
+                        } catch {
+                            usrData.actPath = [];
+                            currentData = data;
+                        }
+                        if (currentData && currentData[args[1]]) {
+                            if (currentData[args[1]].length || Object.keys(currentData[args[1]]).length == 0) {
+                                delete currentData[args[1]];
+                            } else {
+                                console.log("Sorry, please don't delete items with sub items.");
+                            }
+                        } else {
+                            console.log("Sorry, that index does not exist.");
+                        }
+                    } else {
+                        console.log("Please add an index.");
+                    }
+                    rl.close();
+                    loop();
+                    break;
+            }
+        });
+        fs.writeFileSync(usrData.resumePath, JSON.stringify(data));
+    }
 }
 loop();
 
